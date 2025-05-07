9472357621bd3e9d63f8268e63d791a9c76a349e
Nathan Strobl <nas4635@rit.edu>
2025-05-07 16:27:40 -0400

    Adding Lua files for AwesomeWM.

---
+++ theme.lua
@@ -0,0 +1,20 @@
+local theme = {}
+
+theme.font = "monospace;2"
+
+theme.bg_normal = "#282828"
+theme.bg_focus = "#504945"
+theme.bg_urgent = "#cc241d"
+theme.bg_minimize = "#3c3836"
+
+theme.fg_normal = "#ebdbb2"
+theme.fg_focus = "#ffffff"
+theme.fg_urgent = "#ffffff"
+theme.fg_minimize = "#ffffff"
+
+theme.border_width = 2
+theme.border_normal = "#3c3836"
+theme.border_focus = "#d65d0e"
+theme.border_marked = "#ff0000"
+
+return theme
