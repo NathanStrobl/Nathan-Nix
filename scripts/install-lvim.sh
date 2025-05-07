6a750d5efe6627718d76f7570bdd960477386c6b
NathanStrobl <111808514+NathanStrobl@users.noreply.github.com>
2025-05-07 19:35:51 -0400

    Added custom font to Alacritty.

--- scripts/install-lvim.sh
+++ scripts/install-lvim.sh
@@ -4,7 +4,8 @@ if [ ! -d "$HOME/.local/share/lunarvim" ]; then
     echo "Installing LunarVim..."
     bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/master/utils/installer/install.sh) --no-install-dependencies
     mkdir -p ~/.local/share/fonts
-    cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/SpaceMono/SpaceMonoNerdFontMono-Regular.ttf
+    cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/SpaceMono/Regular/SpaceMonoNerdFontMono-Regular.ttf
+    fc-cache -f -v
 fi
 
 exit
